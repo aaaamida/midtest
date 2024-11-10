cb6ecf1d69ec53731a58da4bc343fcd6e063842d
aaaamida <aaaamida6747@gmail.com>
2024-11-10 19:03:34 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,179 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+        runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+        const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+        @override
+        Widget build(BuildContext context) {
+                return MaterialApp(
+                        title: 'Flutter Demo',
+                        theme: ThemeData(
+                                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                                useMaterial3: true,
+                        ),
+                        home: const MyHomePage(title: 'OpenDownloadManager'),
+                        debugShowCheckedModeBanner: false,
+                );
+        }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+        const MyHomePage({super.key, required this.title});
+        final String title;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+        @override
+        State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+        int selectedItem = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+        void _onSelect(int index) {
+                setState(() {
+                        selectedItem = index;
+                });
+        }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        void inputPopup(BuildContext context) {
+                TextEditingController linkCtl = TextEditingController();
+
+                showDialog(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+
+                                title: const Text("Foo"),
+                                content: SizedBox(
+                                        width: MediaQuery.of(context).size.width / 3,
+                                        child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                        TextField(
+                                                                controller: linkCtl,
+                                                                decoration: const InputDecoration(hintText: "Type here"),
+                                                        )
+                                                ],
+                                        ),
+                                ),
+                                actions: [
+                                        const TextButton(onPressed: null, child: Text("Submit")),
+                                        const SizedBox(width: 15),
+                                        TextButton(
+                                                onPressed: () => Navigator.of(context).pop(),
+                                                child: const Text("Cancel")
+                                        ),
+                                ],
+                        )
+                );
+        }
+
+        @override
+        Widget build(BuildContext context) {
+                return Scaffold(
+                        appBar: AppBar(
+                                title: Text(widget.title),
+                                foregroundColor: const Color(0xFFFFFFFF),
+                                backgroundColor: const Color(0xFF634F88),
+                        ),
+                        // ignore: prefer_const_constructors
+                        body: Center(
+                                // ignore: prefer_const_constructors
+                                child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        // ignore: prefer_const_literals_to_create_immutables
+                                        children: [
+                                                const SizedBox(height: 20),
+                                                // ignore: prefer_const_constructors
+                                                Padding(
+                                                        padding: const EdgeInsets.all(16),
+                                                        child: const Row(
+                                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                                children: [
+                                                                        SizedBox(height: 10),
+                                                                        Text("Hello, world!", style: TextStyle(fontSize: 16),),
+                                                                        
+                                                                        SizedBox(height: 10),
+                                                                ],
+                                                        ),
+                                                ),
+                                                const Divider(thickness: 1, indent: 16, endIndent: 16, color: Color(0xFF000000)),
+                                        ],
+                                ),
+                        ),
+                        bottomNavigationBar: BottomAppBar(
+                                color: const Color(0xFF473763),
+                                child: Padding(
+                                        padding: const EdgeInsets.only(top: 5, left: 10, right: 10),
+                                        child: Row(
+                                                children: [
+                                                        _navIcon(Icons.add, 0),
+                                                        const SizedBox(width: 30),
+                                                        _navIcon(Icons.list_outlined, 1),
+                                                        const SizedBox(width: 30),
+                                                        _navIcon(Icons.library_add_check_outlined, 2),
+                                                ],
+                                        ),
+                                ),
+                        ),
+                        floatingActionButton: FloatingActionButton(
+                                onPressed: () => inputPopup(context),
+                                backgroundColor: const Color(0xFF8B6EBF),
+                                hoverColor: const Color(0xFFB993FF),
+                                tooltip: "Add new...",
+                                child: const Icon(Icons.add, color: Color(0xFFFFFFFF)),
+                        ),
+                );
+        }
+        
+        Widget _navIcon(IconData icon, int index) {
+                bool selected = selectedItem == index;
+
+                return GestureDetector(
+                        onTap: () => _onSelect(index),
+                        child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                        if (selected) Container(
+                                                height: 3,
+                                                width: 3,
+                                                decoration: BoxDecoration(
+                                                        color: const Color(0xFFFFFFFF),
+                                                        borderRadius: BorderRadius.circular(2)
+                                                ),
+                                        ),
+                                        Icon(
+                                                icon, 
+                                                color: 
+                                                        selected 
+                                                        ? const Color(0xFFFFFFFF)
+                                                        : const Color(0xFFB2B2B2),
+                                                size: 25), 
+
+                                ],
+                        ),
+                );
+        }
+}
+
+class DownloadsListItems extends StatelessWidget {
+        const DownloadsListItems({super.key});
+
+        @override
+        Widget build(BuildContext context) {
+                return Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: const Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                        SizedBox(height: 10),
+                                        Text("Hello, world!", style: TextStyle(fontSize: 16),),
+                                        
+                                        SizedBox(height: 10),
+                                ],
+                        ),
+                );
+        }
 }
